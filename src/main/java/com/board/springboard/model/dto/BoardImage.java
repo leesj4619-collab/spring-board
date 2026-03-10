757c9083b47a6a6deff7ed830e072cf50eb036f7
leesj4619 <leesj4619@gmail.com>
2026-03-10 16:30:36 +0900

    board 이미지 다중 업로드

--- src/main/java/com/board/springboard/model/dto/BoardImage.java
+++ src/main/java/com/board/springboard/model/dto/BoardImage.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class BoardImage { 
+public class BoardImage {
     private Integer id;                 // PK
     private Integer board_no;           // board 테이블 FK 외래키 다른 기본키와 테이블 연결을 위한 키
     private String img_path;            // 웹 접근 경로
