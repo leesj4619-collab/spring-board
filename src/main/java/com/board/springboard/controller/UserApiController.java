7b0754e41146a229745b7ffe16f4c1e08ca2cd8f
leesj4619 <leesj4619@gmail.com>
2026-03-18 11:25:42 +0900

    UserApiController.java 이메일중복확인, 인증번호발송, 인증번호확인, 회원가입, 로그인 PostMapping추가 # 인증번호 발송 미완성

--- src/main/java/com/board/springboard/controller/UserApiController.java
+++ src/main/java/com/board/springboard/controller/UserApiController.java
@@ -1,9 +1,13 @@
 package com.board.springboard.controller;
 
+import com.board.springboard.common.CookieUtil;
 import com.board.springboard.model.dto.User;
 import com.board.springboard.model.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,42 @@ import java.util.Map;
 public class UserApiController {
 
     private final UserService userService;
+    private final CookieUtil cookieUtil;
+    @Value("${file.upload.path}")
+    private String uploadPath;
+
+    @PostMapping("/user/check-email")
+    public ResponseEntity<?> 이메일중복확인(@RequestBody  Map<String, String> body) {
+        boolean 중복 = userService.이메일중복체크기능(body.get("email"));
+        return ResponseEntity.ok(Map.of("duplicate",중복)); // 이메일 중복했는데 중복이 맞다면 duplicate - 중복 전달
+    }
+    @PostMapping("/user/send-code")
+    public ResponseEntity<?> 인증번호발송(@RequestBody  Map<String, String> body) {
+        userService.인증번호발송(body.get("email"));
+        return ResponseEntity.ok(Map.of("message","인증번호가 발송되었습니다."));
+    }
+    @PostMapping("/user/check-email")
+    public ResponseEntity<?> 인증번호확인(@RequestBody  Map<String, String> body) {
+        boolean 성공 = userService.인증번호검증(body.get("email"), body.get("code"));
+        if (!성공) return ResponseEntity.badRequest().body(Map.of("message","인증번호가 올바르지 않습니다"));
+        return ResponseEntity.ok(Map.of("message","인증 성공!"));
+    }
+    @PostMapping("/user/register")
+    public ResponseEntity<?> 회원가입(@RequestBody User user) {
+        boolean 성공 = userService.회원가입(user);
+        if (!성공) return ResponseEntity.badRequest().body(Map.of("message", "이미 사용중인 이메일입니다."));
+        return ResponseEntity.ok(Map.of("message","회원가입 완료"));
+    }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<?> 로그인(@RequestBody Map<String, String> body,
+                                 HttpServletResponse response) {
+        Map<String, String> 토큰들 = userService.로그인(body.get("email"), body.get("password"));
+        cookieUtil.추가(response, "access_token", 토큰들.get("accessToken"), 60 * 30);
+        cookieUtil.추가(response, "refresh_token", 토큰들.get("refreshToken"), 60 * 30 * 24 * 14);
+        return ResponseEntity.ok(Map.of("message", "로그인 성공"));
+    }
+
 
     // Todo 9 : 이름으로 이메일 찾기 (fetch() 로 요청 → JSON 응답)
     // POST /user/find-email
